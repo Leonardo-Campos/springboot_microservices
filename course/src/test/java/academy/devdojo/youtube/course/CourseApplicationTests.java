7af221a12efff5954681fe642f6ced8f0ef074be
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-01-31 14:40:04 -0300

    Modularizando o projeto

---
+++ course/src/test/java/academy/devdojo/youtube/course/CourseApplicationTests.java
@@ -0,0 +1,12 @@
+package academy.devdojo.youtube.course;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
