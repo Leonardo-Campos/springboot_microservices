d754fc1b5cc0dbe166baeebc1869992a434a94f8
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-02-02 11:34:59 -0300

    Authentication Service Signed and Encrypted JWT (JWS+JWE)

--- auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
+++ auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
@@ -1,12 +1,11 @@
 package academy.devdojo.youtube.auth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
