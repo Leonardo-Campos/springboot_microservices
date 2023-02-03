251d837a8b5a589b3b993dad7abd16af420197b8
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2023-02-03 12:14:46 -0300

    Authentication Service Signed and Encrypted JWT (JWS+JWE)

--- auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
+++ auth/src/test/java/academy/devdojo/youtube/auth/AuthApplicationTests.java
@@ -1,12 +1,24 @@
 package academy.devdojo.youtube.auth;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AuthApplicationTests {
+public class AuthApplicationTests {
 
-//	@Test
-	void contextLoads() {
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void test() {
+		System.out.println(new BCryptPasswordEncoder().encode("devdojo"));
 	}
 
 }
+
+
